664cfb0618bf004b127404b878eb722a8c0c79ff
Fajar Sidik Maulana <112901028+fzrone1@users.noreply.github.com>
2024-09-07 11:58:13 +0700

    Add files via upload

---
+++ testfajar.py
@@ -0,0 +1,8 @@
+print('##  Test System Engineer junior Fajar  ##')
+print('==================================')
+print()
+ 
+jumlah_deret = int(input('Jumlah deret aritmatika yang diinginkan: '))
+ 
+for i in range(1,jumlah_deret+1):
+  print(i*3-1, end=" ")
